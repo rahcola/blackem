50f4cb2165701346eb350de0710fe150a9d47619
Jani Rahkola <jani.rahkola@gmail.com>
2010-05-05 12:38:42 +0300

    More admin view tweaking for products

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,11 @@
 from blackem.products.models import Product, Manufacturer, Category
 from django.contrib import admin
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'manufacturer', 'category')
+    list_filter = ['category', 'manufacturer']
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
 admin.site.register(Manufacturer)
 
