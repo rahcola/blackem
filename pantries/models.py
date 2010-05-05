2924e705e96e17cb64ca901223df8414032e4461
Jani Rahkola <jani.rahkola@gmail.com>
2010-05-05 14:19:48 +0300

    a Pantry model

--- pantries/models.py
+++ pantries/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Pantry(models.Model):
+    name = models.CharField(max_length=100)
+    owner = modles.ForeignKey('users.User')
+
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Pantries"
+
